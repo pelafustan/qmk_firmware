8589d20b3a54fc367cbe6388620c9158f542f902
J. Patricio Parada G <paradagpatricio@gmail.com>
2022-03-16 14:56:19 -0300

    update some layers and disable some unused capabilities to reduce firmware size

--- keyboards/kprepublic/bm40hsrgb/keymaps/pelafustan/rules.mk
+++ keyboards/kprepublic/bm40hsrgb/keymaps/pelafustan/rules.mk
@@ -7,8 +7,8 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -20,3 +20,5 @@ RGB_MATRIX_DRIVER = WS2812
 TAP_DANCE_ENABLE = yes
 LAYOUTS = planck_mit
 LAYOUTS_HAS_RGB = yes
+
+EXTRAFLAGS += -flto
