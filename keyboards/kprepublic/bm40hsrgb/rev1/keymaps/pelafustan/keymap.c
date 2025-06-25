22f434de2877ee8db09bd2a366125b0713f8c207
Patricio Parada <paradagpatricio@gmail.com>
2025-06-25 19:06:19 -0400

    update personal keymap

--- keyboards/kprepublic/bm40hsrgb/rev1/keymaps/pelafustan/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev1/keymaps/pelafustan/keymap.c
@@ -21,6 +21,7 @@
 
 enum layers {
   _QWERTY,
+  _MODS,
   _LOWER,
   _RAISE,
   _ADJUST,
@@ -71,10 +72,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT_planck_mit(
-    LT(_NUM, KC_TAB), KC_Q,         KC_W,         KC_E,         KC_R,                KC_T,    KC_Y,  KC_U,                KC_I,         KC_O,         KC_P,         KC_BSPC,
-    LCTL_T(KC_ESC),   LSFT_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), KC_F,                KC_G,    KC_H,  KC_J,                LGUI_T(KC_K), LALT_T(KC_L), TD(SCLN_CLN), LCTL_T(KC_ENT),
-    TD(LSFT_CAPS),    KC_Z,         KC_X,         KC_C,         KC_V,                KC_B,    KC_N,  KC_M,                KC_COMM,      KC_DOT,       KC_SLSH,      TD(RSFT_QUOTE),
-    RGB_TOG,          KC_LCTL,      KC_LALT,      KC_LGUI,      LT(_LOWER, KC_LBRC),     KC_SPC,     LT(_RAISE, KC_RBRC), KC_RALT,      KC_PSCR,      KC_RCTL,      KC_BSLS
+    KC_TAB,  KC_Q,      KC_W,    KC_E,    KC_R,                KC_T, KC_Y, KC_U,                KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_ESC,  KC_A,      KC_S,    KC_D,    KC_F,                KC_G, KC_H, KC_J,                KC_K,    KC_L,    KC_SCLN, KC_ENT,
+    KC_LSFT, KC_Z,      KC_X,    KC_C,    KC_V,                KC_B, KC_N, KC_M,                KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+    KC_LCTL, TG(_MODS), KC_LALT, KC_LGUI, LT(_LOWER, KC_LBRC),   KC_SPC,   LT(_RAISE, KC_RBRC), KC_RALT, KC_PSCR, KC_RCTL, KC_BSLS
+  ),
+
+  [_MODS] = LAYOUT_planck_mit(
+    LT(_NUM, KC_TAB), _______,      _______,      _______,      _______,             _______, _______, _______,             _______,      _______,      _______,      _______,
+    LCTL_T(KC_ESC),   LSFT_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), _______,             _______, _______, _______,             LGUI_T(KC_K), LALT_T(KC_L), TD(SCLN_CLN), LCTL_T(KC_ENT),
+    TD(LSFT_CAPS),    _______,      _______,      _______,      _______,             _______, _______, _______,             _______,      _______,      _______,      TD(RSFT_QUOTE),
+    _______,          _______,      _______,      _______,      LT(_LOWER, KC_LBRC),      _______,     LT(_RAISE, KC_RBRC), _______,      _______,      _______,      _______
   ),
 
   /* Lower
